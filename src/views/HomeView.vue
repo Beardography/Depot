7658f7a9be2d6d435987cd4e8c18725ebf1a8589
Beardography <ryan.l.huber@gmail.com>
2023-08-07 20:27:46 -0500

    setting up swatches

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,300 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+<script setup></script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <h1>Colors</h1>
+  <div class="swatches">
+    <div class="swatch"></div>
+    <div class="swatch"></div>
+    <div class="swatch"></div>
+    <div class="swatch"></div>
+    <div class="swatch"></div>
+    <div class="swatch"></div>
+    <div class="swatch"></div>
+    <div class="swatch"></div>
+    <div class="swatch"></div>
+  </div>
+
+  <div class="swatches">
+    <div class="swatch"></div>
+    <div class="swatch"></div>
+    <div class="swatch"></div>
+    <div class="swatch"></div>
+    <div class="swatch"></div>
+    <div class="swatch"></div>
+    <div class="swatch"></div>
+    <div class="swatch"></div>
+    <div class="swatch"></div>
+  </div>
+
+  <div class="swatches">
+    <div class="swatch"></div>
+    <div class="swatch"></div>
+    <div class="swatch"></div>
+    <div class="swatch"></div>
+    <div class="swatch"></div>
+    <div class="swatch"></div>
+    <div class="swatch"></div>
+    <div class="swatch"></div>
+    <div class="swatch"></div>
+  </div>
+
+  <div class="swatches">
+    <div class="swatch"></div>
+    <div class="swatch"></div>
+    <div class="swatch"></div>
+    <div class="swatch"></div>
+    <div class="swatch"></div>
+    <div class="swatch"></div>
+    <div class="swatch"></div>
+    <div class="swatch"></div>
+    <div class="swatch"></div>
+  </div>
+
+  <div class="swatches">
+    <div class="swatch"></div>
+    <div class="swatch"></div>
+    <div class="swatch"></div>
+    <div class="swatch"></div>
+    <div class="swatch"></div>
+    <div class="swatch"></div>
+    <div class="swatch"></div>
+    <div class="swatch"></div>
+    <div class="swatch"></div>
+  </div>
+
+  <div class="swatches">
+    <div class="swatch"></div>
+    <div class="swatch"></div>
+    <div class="swatch"></div>
+    <div class="swatch"></div>
+    <div class="swatch"></div>
+    <div class="swatch"></div>
+    <div class="swatch"></div>
+    <div class="swatch"></div>
+    <div class="swatch"></div>
+  </div>
 </template>
+
+<!-- <style lang="scss" scoped>
+.swatches {
+  display: flex;
+  flex-wrap: wrap;
+
+  .swatch {
+    width: 100px;
+    height: 100px;
+    // border: 1px solid black;
+    margin: 5px;
+  }
+
+  &:nth-of-type(1) {
+    .swatch {
+      // background-color: purple;
+
+      &:nth-of-type(1) {
+        background-color: $green;
+      }
+
+      &:nth-of-type(2) {
+        background-color: red;
+      }
+
+      &:nth-of-type(3) {
+        background-color: red;
+      }
+
+      &:nth-of-type(4) {
+        background-color: red;
+      }
+
+      &:nth-of-type(5) {
+        background-color: red;
+      }
+
+      &:nth-of-type(6) {
+        background-color: red;
+      }
+
+      &:nth-of-type(7) {
+        background-color: red;
+      }
+
+      &:nth-of-type(8) {
+        background-color: red;
+      }
+
+      &:nth-of-type(9) {
+        background-color: red;
+      }
+    }
+  }
+
+  &:nth-of-type(2) {
+    .swatch {
+      background-color: brown;
+
+      &:nth-of-type(1) {
+        background-color: red;
+      }
+
+      &:nth-of-type(2) {
+        background-color: red;
+      }
+
+      &:nth-of-type(3) {
+        background-color: red;
+      }
+
+      &:nth-of-type(4) {
+        background-color: red;
+      }
+
+      &:nth-of-type(5) {
+        background-color: red;
+      }
+
+      &:nth-of-type(6) {
+        background-color: red;
+      }
+
+      &:nth-of-type(7) {
+        background-color: red;
+      }
+
+      &:nth-of-type(8) {
+        background-color: red;
+      }
+
+      &:nth-of-type(9) {
+        background-color: red;
+      }
+    }
+  }
+
+  &:nth-of-type(3) {
+    .swatch {
+      background-color: brown;
+
+      &:nth-of-type(1) {
+        background-color: red;
+      }
+
+      &:nth-of-type(2) {
+        background-color: red;
+      }
+
+      &:nth-of-type(3) {
+        background-color: red;
+      }
+
+      &:nth-of-type(4) {
+        background-color: red;
+      }
+
+      &:nth-of-type(5) {
+        background-color: red;
+      }
+
+      &:nth-of-type(6) {
+        background-color: red;
+      }
+
+      &:nth-of-type(7) {
+        background-color: red;
+      }
+
+      &:nth-of-type(8) {
+        background-color: red;
+      }
+
+      &:nth-of-type(9) {
+        background-color: red;
+      }
+    }
+  }
+
+  &:nth-of-type(4) {
+    .swatch {
+      background-color: brown;
+
+      &:nth-of-type(1) {
+        background-color: red;
+      }
+
+      &:nth-of-type(2) {
+        background-color: red;
+      }
+
+      &:nth-of-type(3) {
+        background-color: red;
+      }
+
+      &:nth-of-type(4) {
+        background-color: red;
+      }
+
+      &:nth-of-type(5) {
+        background-color: red;
+      }
+
+      &:nth-of-type(6) {
+        background-color: red;
+      }
+
+      &:nth-of-type(7) {
+        background-color: red;
+      }
+
+      &:nth-of-type(8) {
+        background-color: red;
+      }
+
+      &:nth-of-type(9) {
+        background-color: red;
+      }
+    }
+  }
+
+  &:nth-of-type(5) {
+    .swatch {
+      background-color: brown;
+
+      &:nth-of-type(1) {
+        background-color: red;
+      }
+
+      &:nth-of-type(2) {
+        background-color: red;
+      }
+
+      &:nth-of-type(3) {
+        background-color: red;
+      }
+
+      &:nth-of-type(4) {
+        background-color: red;
+      }
+
+      &:nth-of-type(5) {
+        background-color: red;
+      }
+
+      &:nth-of-type(6) {
+        background-color: red;
+      }
+
+      &:nth-of-type(7) {
+        background-color: red;
+      }
+
+      &:nth-of-type(8) {
+        background-color: red;
+      }
+
+      &:nth-of-type(9) {
+        background-color: red;
+      }
+    }
+  }
+}
+</style> -->
