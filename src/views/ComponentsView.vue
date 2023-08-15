f9dedee1e1573ab5a253a80353975d228224afab
Beardography <ryan.l.huber@gmail.com>
2023-08-15 15:04:05 -0500

    surfacing Link component

--- src/views/ComponentsView.vue
+++ src/views/ComponentsView.vue
@@ -1,3 +1,8 @@
+<script setup>
+import Link from '../components/Link.vue'
+</script>
+
 <template>
   <h1>Components</h1>
+  <Link target="_blank" href="https://www.google.com" text="this is a link" />
 </template>
