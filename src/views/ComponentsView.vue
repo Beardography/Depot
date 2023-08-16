9654bf83afda0c054005eb0fb40feb65ec063824
Beardography <ryan.l.huber@gmail.com>
2023-08-15 19:01:09 -0500

    updated component page

--- src/views/ComponentsView.vue
+++ src/views/ComponentsView.vue
@@ -4,5 +4,6 @@ import Link from '../components/Link.vue'
 
 <template>
   <h1>Components</h1>
+  <h3>Link Component</h3>
   <Link target="_blank" href="https://www.google.com" text="this is a link" />
 </template>
