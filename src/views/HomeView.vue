281c3f052d1a5e9a710e276355096eaa95bbd2b2
Beardography <ryan.l.huber@gmail.com>
2023-09-29 18:07:06 -0500

    added route to sandbox page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,5 +5,6 @@
     <h1><router-link to="/type">Type</router-link></h1>
     <h1><router-link to="/colors">Colors</router-link></h1>
     <h1><router-link to="/components">Components</router-link></h1>
+    <h1><router-link to="/sandbox">Sandbox</router-link></h1>
   </div>
 </template>
