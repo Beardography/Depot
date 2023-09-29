90582ed689fe1f82e126dfc61b3cb2237e6e3733
Beardography <ryan.l.huber@gmail.com>
2023-09-29 18:06:58 -0500

    created sandbox page for building new components

---
+++ src/views/SandboxView.vue
@@ -0,0 +1,9 @@
+<script setup>
+import Button from '../components/Button.vue'
+</script>
+
+<template>
+  <Button size="lg" text="Add to cart" />
+  <Button size="md" text="Add to cart" />
+  <Button size="sm" text="Add to cart" />
+</template>
