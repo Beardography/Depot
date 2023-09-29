a80897ef6044eb1400845a992b0eac9b0306b1ee
Beardography <ryan.l.huber@gmail.com>
2023-09-29 18:06:18 -0500

    setting up button component

---
+++ src/components/Button.vue
@@ -0,0 +1,19 @@
+<script setup>
+defineProps({
+  text: {
+    type: String,
+    default: 'Button'
+  },
+  size: {
+    type: String,
+    options: ['small', 'sm', 'medium', 'md', 'large', 'lg'],
+    default: 'medium'
+  }
+})
+</script>
+
+<template>
+  <button :class="size">
+    {{ text }}
+  </button>
+</template>
