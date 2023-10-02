2c6b2640e0f5a890539af116da6b8166261b286d
Beardography <ryan.l.huber@gmail.com>
2023-10-02 17:55:09 -0500

    set up tags

---
+++ src/components/Tag.vue
@@ -0,0 +1,27 @@
+<script setup>
+defineProps({
+  text: {
+    type: String,
+    default: 'Tag'
+  },
+  size: {
+    type: String,
+    options: ['medium', 'md', 'large', 'lg'],
+    default: 'medium'
+  },
+  status: {
+    type: String,
+    options: ['success', 'error', 'warning', 'waiting', 'processing', 'info']
+  },
+  color: {
+    type: String,
+    options: ['orange', 'purple', 'blue', 'green', 'yellow', 'red', 'default']
+  }
+})
+</script>
+
+<template>
+  <div class="tag" :class="[size, status, color]">
+    {{ text }}
+  </div>
+</template>
